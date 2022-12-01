661bd704c28b4e299bf4e1c6c3b60166d2c43274
KseniTi <lioness2006@bk.ru>
2022-12-01 17:43:52 +0300

    Prog034

---
+++ Prog034/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+
+void Zadacha034()
+{
+    int size = 5;
+    int[] numbers = new int[size];
+    FillArray(numbers, 100, 1000);
+    PrintArray(numbers);
+
+    int count = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if(numbers[i] % 2 == 0) count++;
+    }
+    Console.WriteLine(count);
+}
+
+void FillArray(int[] nums, int minValue, int maxValue)
+{
+    Random rand = new Random();
+    int length = nums.Length;
+    for (int i = 0; i < length; i++)
+    {
+        nums[i] = rand.Next(minValue, maxValue);
+    }
+}
+
+void PrintArray(int[] nums)
+{
+    int length = nums.Length;
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write(nums[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+Zadacha034();
