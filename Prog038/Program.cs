5285e3d23999a6bb3ebbd4d7d6548d25876eb8ac
KseniTi <lioness2006@bk.ru>
2022-12-01 18:13:30 +0300

    Prog038

---
+++ Prog038/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Задача 38: Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементов массива.
+[3,21 7,04 22,93 -2,71 78,24] -> 80,95
+*/
+
+void Zadacha038()
+{
+    double[] numbers = {3.21, 7.04, 22.93, -2.71, 78.24};
+    PrintArray(numbers);
+    
+    double minNumber = numbers[0];
+    double maxNumber = numbers[0];
+    for(int i = 1; i < numbers.Length; i++)
+    {
+        if(numbers[i] < minNumber) minNumber = numbers[i];
+        if(numbers[i] > maxNumber) maxNumber = numbers[i]; 
+    }
+    Console.WriteLine(Math.Round((maxNumber - minNumber), 2));
+}
+
+void PrintArray(double[] nums)
+{
+    int length = nums.Length;
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write(nums[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+Zadacha038();
